5f6090d4614c4bb5834091a5b180456e48ec9be6
Dru Serkes <DruSerkes@gmail.com>
2020-07-31 13:36:08 -0400

    Updates

---
+++ precache-manifest.33bb303dca1d3e2ce58208d6c0fe4bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0286f8c03cf4e8a8f81aa39b5a7208",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "cfeb75111d770b0ab421",
+    "url": "/Todo-list/static/css/main.1e309884.chunk.css"
+  },
+  {
+    "revision": "b23882a0927a68e8f6bd",
+    "url": "/Todo-list/static/js/2.bc423b6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list/static/js/2.bc423b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfeb75111d770b0ab421",
+    "url": "/Todo-list/static/js/main.d9b41c4a.chunk.js"
+  },
+  {
+    "revision": "3cf7833c1834a2eda9f5",
+    "url": "/Todo-list/static/js/runtime-main.dda2be71.js"
+  }
+]);
